e5d6442a49f0ffc8a87862603ee6faa2a63d2654
Serega5555 <lazarpalace@gmail.com>
2025-09-24 11:33:00 +0300

    Create UserViewModel.cs

---
+++ Monkeynetwork/ViewModels/Account/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Monkeynetwork.Models.Users;
+
+namespace Monkeynetwork.ViewModels.Account
+{
+    public class UserViewModel
+    {
+        public User User { get; set; }
+
+        public UserViewModel(User user)
+        {
+            User = user;
+        }
+
+        public List<User> Friends { get; set; }
+    }
+}
