0f3ae6fb1c7e0b97e1b4ff492d54b0b8ff471155
Serega5555 <lazarpalace@gmail.com>
2025-09-24 11:35:58 +0300

    Create UserWithFriendExt.cs

---
+++ Monkeynetwork/ViewModels/Account/UserWithFriendExt.cs
@@ -0,0 +1,9 @@
+﻿using Monkeynetwork.Models.Users;
+
+namespace Monkeynetwork.ViewModels.Account
+{
+    public class UserWithFriendExt : User
+    {
+        public bool IsFriendWithCurrent { get; set; }
+    }
+}
