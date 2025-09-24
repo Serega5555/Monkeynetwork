bda4eef0117edd2e86f4f0cf137d07e0367a4c90
Serega5555 <lazarpalace@gmail.com>
2025-09-24 10:43:45 +0300

    Create MessageConfuiguration.cs

---
+++ Monkeynetwork/Configs/MessageConfuiguration.cs
@@ -0,0 +1,15 @@
+﻿using Monkeynetwork.Models.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Monkeynetwork.Configs
+{
+    public class MessageConfuiguration : IEntityTypeConfiguration<Message>
+    {
+        public void Configure(EntityTypeBuilder<Message> builder)
+        {
+            builder.ToTable("Messages").HasKey(p => p.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+        }
+    }
+}
