457488cd169c235f2dc0abad6a9b40aee43a2bc5
Serega5555 <lazarpalace@gmail.com>
2025-09-24 12:51:04 +0300

    Create MappingProfile.cs

---
+++ Monkeynetwork/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Monkeynetwork.ViewModels.Account;
+using Monkeynetwork.Models.Users;
+
+namespace Monkeynetwork
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RegisterViewModel, User>()
+               .ForMember(x => x.BirthDate, opt => opt.MapFrom(c => new DateTime((int)c.Year, (int)c.Month, (int)c.Date)))
+               .ForMember(x => x.Email, opt => opt.MapFrom(c => c.EmailReg))
+               .ForMember(x => x.UserName, opt => opt.MapFrom(c => c.Login));
+            CreateMap<LoginViewModel, User>();
+
+            CreateMap<UserEditViewModel, User>();
+            CreateMap<User, UserEditViewModel>().ForMember(x => x.UserId, opt => opt.MapFrom(c => c.Id));
+
+            CreateMap<UserWithFriendExt, User>();
+            CreateMap<User, UserWithFriendExt>();
+        }
+    }
+}
