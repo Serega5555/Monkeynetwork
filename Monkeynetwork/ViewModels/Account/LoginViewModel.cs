41faeee9d01b651dbaf66f3aec36028bcb2d30b9
Serega5555 <lazarpalace@gmail.com>
2025-09-24 11:28:57 +0300

    Create LoginViewModel.cs

---
+++ Monkeynetwork/ViewModels/Account/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Monkeynetwork.ViewModels.Account
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        [Display(Name = "Сохранить вход")]
+        public bool RememberMe { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
