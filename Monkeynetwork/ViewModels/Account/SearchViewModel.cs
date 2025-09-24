cc9c4302ce70a37a24aa8d8b47d1dbad35ec3ec6
Serega5555 <lazarpalace@gmail.com>
2025-09-24 11:31:42 +0300

    Create SearchViewModel.cs

---
+++ Monkeynetwork/ViewModels/Account/SearchViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Monkeynetwork.ViewModels.Account
+{
+    public class SearchViewModel
+    {
+        public List<UserWithFriendExt> UserList { get; set; }
+
+    }
+}
