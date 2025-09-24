e4508dd0db88de5db0690e363c6be05881757a71
Serega5555 <lazarpalace@gmail.com>
2025-09-24 10:51:36 +0300

    Create IRepository.cs

---
+++ Monkeynetwork/Data/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Monkeynetwork.Data.Repository.Repository
+{
+    public interface IRepository<T>
+        where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(T id);
+    }
+}
