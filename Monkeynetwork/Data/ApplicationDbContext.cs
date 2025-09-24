d977118f19b9a5f0d0322af1682e2a781b258b7c
Serega5555 <lazarpalace@gmail.com>
2025-09-24 10:56:14 +0300

    Create ApplicationDbContext.cs

---
+++ Monkeynetwork/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Monkeynetwork.Configs;
+using Monkeynetwork.Models.Users;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Monkeynetwork.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration<Friend>(new FriendConfiguration());
+            builder.ApplyConfiguration(new MessageConfuiguration());
+        }
+    }
+}
