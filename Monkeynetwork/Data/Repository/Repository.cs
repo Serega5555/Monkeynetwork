2617afb361915c103b02e4943f051faad752ed4d
Serega5555 <lazarpalace@gmail.com>
2025-09-24 10:53:22 +0300

    Create Repository.cs

---
+++ Monkeynetwork/Data/Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Monkeynetwork.Data.Repository.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Monkeynetwork.Data.Repository
+{
+    public class Repository<T> : IRepository<T>
+        where T : class
+    {
+        protected ApplicationDbContext _db;
+
+        public DbSet<T> Set { get; private set; }
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            var set = _db.Set<T>();
+            set.Load();
+
+            Set = set;
+        }
+
+        public void Create(T item)
+        {
+            Set.Add(item);
+            _db.SaveChanges();
+        }
+
+        public void Delete(T item)
+        {
+            Set.Remove(item);
+            _db.SaveChanges();
+        }
+
+        public T Get(int id)
+        {
+            return Set.Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return Set;
+        }
+
+        public void Update(T item)
+        {
+            Set.Update(item);
+            _db.SaveChanges();
+        }
+    }
+}
